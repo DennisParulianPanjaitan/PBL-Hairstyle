dc8fce011dcb339e8a6ebe999e2db6837e87426c
AfrizalDS <angelicacoustarika@gmail.com>
2024-11-19 03:21:36 +0700

    Feat: ADD BottomMenu

--- Hairstyle/lib/screens/home_page.dart
+++ Hairstyle/lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/bottom_navbar.Dart';
 import 'camera_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -21,60 +22,10 @@ class _HomePageState extends State<HomePage> {
       body: Center(
         child: Text('Tab $_selectedIndex selected'),
       ),
-      bottomNavigationBar: Padding(
-        padding: EdgeInsets.only(bottom: 8.0),
-        child: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          notchMargin: 8.0,
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home, size: 21.0),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.grid_view, size: 21.0),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: SizedBox.shrink(),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite, size: 21.0),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person, size: 21.0),
-                label: '',
-              ),
-            ],
-          ),
-        ),
+      bottomNavigationBar: BottomNavBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
-      floatingActionButton: SizedBox(
-        height: 48.0,
-        width: 48.0,
-        child: FloatingActionButton(
-          onPressed: () {
-            // Navigasi ke CameraPage tanpa passing controller
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CameraPage(),
-              ),
-            );
-          },
-          child: Icon(Icons.camera_alt, size: 24.0),
-          backgroundColor: Color(0xFF1B1A55),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
