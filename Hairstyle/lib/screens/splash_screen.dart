5974c1ebf2a57f88f734626afbab8f447056bab8
AfrizalDS <angelicacoustarika@gmail.com>
2024-11-22 21:59:08 +0700

    FIX : Edit Splash Screen

--- Hairstyle/lib/screens/splash_screen.dart
+++ Hairstyle/lib/screens/splash_screen.dart
@@ -26,6 +26,11 @@ class SplashScreen extends StatelessWidget {
               child: Text(
                 "Discover the hairstyle\nthat matches your face",
                 textAlign: TextAlign.center, // Align text to center
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
           ),
@@ -37,61 +42,63 @@ class SplashScreen extends StatelessWidget {
                   TopRightCornerClipper(), // Custom clipper for top right corner
               child: Container(
                 height: MediaQuery.of(context).size.height *
-                    0.31, // Covers 31% of screen
+                    0.35, // Covers 35% of screen
                 width: double.infinity,
                 color: Colors.white,
                 padding: EdgeInsets.symmetric(
                     horizontal: 24.0,
                     vertical: 20.0), // Adjust padding to fit lower area
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Spacer(),
-                    Text(
-                      "HairMate",
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Scan, Style, and Shine with HairMate! Effortlessly discover the perfect hairstyle tailored just for you and elevate your look with confidence every day.",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.black,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Navigasi ke halaman login
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginPage()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFF1B1A55), // Button color
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 50, vertical: 15),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                          ),
-                        ),
-                        child: Text(
-                          "Get Started",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "HairMate",
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(height: 10),
+                      Text(
+                        "Scan, Style, and Shine with HairMate! Effortlessly discover the perfect hairstyle tailored just for you and elevate your look with confidence every day.",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: double
+                            .infinity, // Lebar penuh sesuai dengan parent container
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Navigasi ke halaman login
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF1B1A55), // Warna tombol
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            "Get Started",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
