bcc8c07747133e02b2b64c6a5bd684b2e70a9da6
pdh-24 <muhammadpaksi.mp@gmail.com>
2024-11-30 13:25:21 +0700

    Backend dengan db remote

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,16 +1,13 @@
-// Load environment variables from the .env file
-import dotenv from 'dotenv';
-
-// Load environment variables
-dotenv.config();
+import dotenv from 'dotenv';  // Load environment variables from the .env file
 
 import { Router } from 'express';
 import { compare } from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { sql } from '@vercel/postgres';
+import { getUserByUserName } from '../databases/populate.postgre.js';
 
 // const supabase = createClient('https://vcixgtpgalxcdxxgdxpe.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjaXhndHBnYWx4Y2R4eGdkeHBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI3NzQ5NTEsImV4cCI6MjA0ODM1MDk1MX0.qZa9Sxw7nBIssb1zVMEkVqcGPJ0hIBQxXlvYyZ618Uw');
-dotenv.config({ path: '.env.local' }); // Load environment variables
+dotenv.config();  // Load environment variables
+dotenv.config({ path: '.env.development.local' }); // Load environment variables
 
 const { sign, verify } = jwt;
 const router = Router();
@@ -28,9 +25,10 @@ const SECRET_KEY = process.env.JWT_SECRET_KEY;
 // const SECRET_KEY = "3938e9e111082bcba44f9d19461580d11177e5af410a485ea417103791fa7ce78656d3bd4651835cf2de75bb2a889717fd0eea7319733fc8c5ffff3f3032c655";
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
-  
+
+    const userData = getUserByUserName(username);
     // Find user by username or email
-    const user = users.find((u) => u.username === username || u.email === username);
+    const user = (await userData).find((user) => user['username'] === username || user['email'] === username);
     if (!user) {
       return res.status(401).json({ message: 'Username salah' });
     }
