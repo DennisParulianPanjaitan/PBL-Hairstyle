3616b0e652bc980f5bd78b94bc14fd2ca6efc9ff
Afrizal Dwi Septian <113771962+AfrizalDS@users.noreply.github.com>
2024-12-02 22:25:23 +0700

    ADD : Adding widget hair button

---
+++ Hairstyle/lib/widgets/hair_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HairButton extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  HairButton({
+    required this.label,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 115, // Menentukan lebar tetap untuk button
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+        decoration: BoxDecoration(
+          color: isSelected ? Color(0xFF1B1A55) : Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(color: Color(0xFF1B1A55)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment
+              .center, // Mengatur posisi ikon dan teks agar sejajar
+          children: [
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w900,
+                color: isSelected ? Colors.white : Color(0xFF1B1A55),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
