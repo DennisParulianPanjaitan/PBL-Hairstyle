9ce10d00fce761989050425d9204af0fe01a1dba
pdh-24 <muhammadpaksi.mp@gmail.com>
2024-12-09 06:47:14 +0700

    Tes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -22,6 +22,29 @@ const users = [
 ];
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+router.post('/registrasi', async (req, res) => {
+  const { username, email, password } = req.body;
+    
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: 'Seluruh isian harus diisi' });
+  }
+  const [ checkUsername ] = await db.query(`SELECT id FROM users WHERE username = '${username}'`);
+    
+  if (checkUsername.length > 0) {
+    return res.status(400).json({ error: 'Username sudah terdaftar' });
+  }
+  
+  try {
+    await db.query(`
+      INSERT INTO users (username, email, password, status)
+        VALUES ('${username}', '${email}', '${password}','activated');
+    `);
+    res.status(200).json({ message: 'Registrasi berhasil', berhasil: true});
+  } catch (err) {
+    res.status(500).json({ error: `Kesalahan:\n${err}` });
+  }
+});
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
