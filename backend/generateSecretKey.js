7f01a22265ce9ff2babcdcdc86cf5879927d03c6
pdh-24 <muhammadpaksi.mp@gmail.com>
2024-12-13 00:11:06 +0700

    Delete express.js backend

--- backend/generateSecretKey.js
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from 'crypto';
-
-// Generate a 64-byte random hex string
-const secretKey = randomBytes(64).toString('hex');
-console.log('Generated Secret Key:', secretKey);
