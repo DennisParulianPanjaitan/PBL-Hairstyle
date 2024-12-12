f7b1d1d830e68667217651775ab7f029abf1692b
pdh-24 <muhammadpaksi.mp@gmail.com>
2024-12-13 00:01:22 +0700

    Hapus dir backend Express.js

--- backend/utils/generateOTP.js
+++
@@ -1,6 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-export default function generateOTP() {
-  const generate = customAlphabet('123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', 4);
-  return generate;
-};
